059dd27d4dc5edf3218dbcf41a5f7ed2c1dbf3f6
Xavier Jannin <xav.jann1@gmail.com>
2020-01-15 17:24:03 +0100

    Ajout de topics à la classe VelocityJoint pour récupérer des informations et contrôler le joint (#8 #10)

--- STM32/Core/Inc/robot_VelocityJoint.h
+++ STM32/Core/Inc/robot_VelocityJoint.h
@@ -5,6 +5,7 @@
 #include "robot_PID.h"
 #include "robot_Motor.h"
 #include "robot_Encoder.h"
+#include "std_msgs/Bool.h"
 
 namespace robot {
 
@@ -60,11 +61,17 @@ public:
   }
 
   // Joint:
+  void addJointSubscriber(ros::NodeHandle& nh);
+  void addJointPublishers(ros::NodeHandle& nh);
+  void publishData();
   float getVelocity() {
     return encoder_.getVelocity() * ratio_;
   }
   float getPosition() {
     return encoder_.getPosition() * ratio_;
+  }
+  float getPWM() {
+    return pwm_;
   }
 
 private:
@@ -74,6 +81,33 @@ private:
 
   float cmd_vel_ = 0;
   float ratio_ = 1;
+  float pwm_ = 0;
+
+  // Subscribers:
+  std::string set_command_name_;
+  ros::Subscriber<std_msgs::Float32, robot::VelocityJoint> set_command_sub_;
+  void set_command_cb(const std_msgs::Float32 &msg) { cmd_vel_ = msg.data; }
+
+  std::string on_off_name_;
+  ros::Subscriber<std_msgs::Bool, robot::VelocityJoint> on_off_sub_;
+  void on_off_cb(const std_msgs::Bool &msg) {
+    msg.data ? motor_.on() : motor_.off();
+  }
+
+  // Publishers'name:
+  std::string pos_name_;
+  std::string vel_name_;
+  std::string pwm_name_;
+
+  // Publishers:
+  ros::Publisher pos_pub_;
+  ros::Publisher vel_pub_;
+  ros::Publisher pwm_pub_;
+
+  // Publishers' message:
+  std_msgs::Float32 pos_msg_;
+  std_msgs::Float32 vel_msg_;
+  std_msgs::Float32 pwm_msg_;
 
   float getDeltaTicks();
 };
--- STM32/Core/Src/robot_VelocityJoint.cpp
+++ STM32/Core/Src/robot_VelocityJoint.cpp
@@ -6,13 +6,44 @@ VelocityJoint::VelocityJoint(EncoderDef enc_def, PidDef pid_def, MotorDef mot_de
     encoder_(enc_def, joint_def.name + "/encoder"),
     motor_(mot_def),
     pid_(pid_def, joint_def.name + "/pid"),
-    cmd_vel_(0), ratio_(joint_def.ratio) {
+    cmd_vel_(0), ratio_(joint_def.ratio),
+
+    set_command_name_(joint_def.name + "/command"),
+    set_command_sub_(set_command_name_.c_str(), &VelocityJoint::set_command_cb, this),
+    on_off_name_(joint_def.name + "/on_off"),
+    on_off_sub_(on_off_name_.c_str(), &VelocityJoint::on_off_cb, this),
+
+    pos_name_(joint_def.name + "/pos"), vel_name_(joint_def.name + "/vel"), pwm_name_(joint_def.name + "/pwm"),
+    pos_pub_(pos_name_.c_str(), &pos_msg_),
+    vel_pub_(vel_name_.c_str(), &vel_msg_),
+    pwm_pub_(pwm_name_.c_str(), &pwm_msg_) {
 }
 
 void VelocityJoint::init() {
   encoder_.init();
   motor_.init();
   getDeltaTicks();
+}
+
+void VelocityJoint::addJointSubscriber(ros::NodeHandle& nh) {
+  nh.subscribe(set_command_sub_);
+  nh.subscribe(on_off_sub_);
+}
+
+void VelocityJoint::addJointPublishers(ros::NodeHandle& nh) {
+  nh.advertise(pos_pub_);
+  nh.advertise(vel_pub_);
+  nh.advertise(pwm_pub_);
+}
+
+void VelocityJoint::publishData() {
+  pos_msg_.data = getPosition();
+  vel_msg_.data = getVelocity();
+  pwm_msg_.data = pwm_;
+
+  pos_pub_.publish(&pos_msg_);
+  vel_pub_.publish(&vel_msg_);
+  pwm_pub_.publish(&pwm_msg_);
 }
 
 float VelocityJoint::update() {
@@ -26,10 +57,10 @@ float VelocityJoint::update() {
   float effort = pid_.updatePid(vel - cmd_vel_, dt);
 
   // Motor:
-  float pwm = effort;
-  motor_.turn(pwm);
+  pwm_ = effort;
+  motor_.turn(pwm_);
 
-  return pwm;
+  return pwm_;
 }
 
 float VelocityJoint::getDeltaTicks() {
